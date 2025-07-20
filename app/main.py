8582ba5ccc729c074ab292035c79f9d166dd9401
Makmudul0 <makmudulmm@gmail.com>
2025-07-20 17:06:38 +0600

    Fix linting issues for flake8

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     try:
@@ -19,6 +20,7 @@ def home():
         return f"Connected to PostgreSQL: {db_version}"
     except Exception as e:
         return f"Database connection error: {e}"
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
